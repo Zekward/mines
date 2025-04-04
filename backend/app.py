625dbde2774bdebd1a0a7de1d6c211e19d0a78a4
Zekey <zh437@cornell.edu>
2025-04-04 04:17:14 -0400

    Improve signup page style

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,35 @@
-from flask import Flask  
+from flask import Flask, render_template, request
+import webbrowser
+from threading import Timer
 
 app = Flask(__name__)
+users = {}
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username not in users:
+            return 'User not found'
+        elif users[username] == password:
+            return 'Login successful'
+        else: 
+            return 'Invalid username or password'
+
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        users[username] = password
+        
+        return 'Signup successful!'
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    Timer(1, lambda: webbrowser.open('http://127.0.0.1:5000')).start()
+    app.run(debug=True)
